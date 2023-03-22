ea34f0f9ff75b5f4e06b9b61e742ca7a3bb486ca
Beatriz Angelita <beatrizelzi@gmail.com>
2023-03-22 13:58:16 -0300

    Novo Projeto - Conta Bancaria

---
+++ contaBancaria/src/conta/Menu.java
@@ -0,0 +1,13 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+	}
+
+}
---
+++ contaBancaria/src/conta/util/Cores.java
@@ -0,0 +1,13 @@
+package conta.util;
+
+import java.util.Scanner;
+
+public class Cores {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+	}
+
+}
