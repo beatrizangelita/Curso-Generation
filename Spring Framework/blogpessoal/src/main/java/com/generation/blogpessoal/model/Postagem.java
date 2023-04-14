1621db96fe9a066a82967256661b26b666e0975f
Beatriz Angelita <beatrizelzi@gmail.com>
2023-04-14 17:10:48 -0300

    BlogPessoal - CRUD

--- Spring Framework/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ Spring Framework/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,3 +1,6 @@
+// Model --> modelagem do banco de dados(criação das tabelas)
+
+
 package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
@@ -16,21 +19,23 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Table(name = "tb_postagens")
 public class Postagem {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id // informando que é a chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // para que seja gerado automaticamente
 	private Long id;
 	
-	@NotBlank(message = "O atributo título é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-	private String título;
+	@NotBlank(message = "O atributo título é Obrigatório!") // NotNull
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres") // para informar o valor minino e maximo
+	private String titulo;
 	
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo texto deve conter no mínimo 05 e no máximo 100 caracteres")
 	private String texto;
 	
-	@UpdateTimestamp
-	private LocalDateTime data;
+	@UpdateTimestamp // dar um update automaticamente na data
+	private LocalDateTime data; 
 
+	
+	// Getteres and Setteres 
 	public Long getId() {
 		return id;
 	}
@@ -39,12 +44,12 @@ public class Postagem {
 		this.id = id;
 	}
 
-	public String getTítulo() {
-		return título;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setTítulo(String título) {
-		this.título = título;
+	public void setTítulo(String titulo) {
+		this.titulo = titulo;
 	}
 
 	public String getTexto() {
--- Spring Framework/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ Spring Framework/blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,12 +1,20 @@
+// Repository --> consultas no banco de dados 
+
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 @Repository
 
+// Para conseguir acessar a tabela Postagem
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
-	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 }
+
